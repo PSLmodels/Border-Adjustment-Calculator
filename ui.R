ad6c45c0f7a8ff3899d919999d8bc1a07fb090ae
Sean <zhwa@umich.edu>
2016-12-22 12:51:15 -0600

    modified intros, and changes default values

--- ui.R
+++ ui.R
@@ -4,30 +4,31 @@ fluidPage(
   tags$h2("How Border Adjustments Work"),
   fluidRow(
     column(3,
-           wellPanel(strong("This example illustrates how a border adjustment would affect a firm with $100 in sales 
-                             and the other characteristics supplied by the user. Under full currency adjustment, the 
-                             dollar appreciates by the same rate as the tax -- this effect is predicted by efficient 
-                             market models. This simple model assumes higher import costs are not passed through to 
-                             consumers when the currency adjustment is incomplete, and it assumes that firms 
-                             can fully deduct losses. ")),
+           wellPanel(strong("This example illustrates how a border adjustment would affect a firm with $100 
+                            in initial sales and other characteristics supplied by the user. Under full 
+                            currency adjustment, the dollar appreciates by the same rate as the tax -- 
+                            this effect is predicted by efficient market models. Under partial currency 
+                            adjustments, firms may be able to pass on some of their increased import 
+                            costs to consumers.  This model assumes that firms can fully deduct losses 
+                            against past and future taxes.")),
       wellPanel(
         sliderInput("c8", "Tax Rate (%)",
-                  min=0, max=100, value=50),
+                  min=0, max=100, value=20),
         strong("Note the current law assumes a tax rate of 35%, with no Border adjustment.")),
       wellPanel(
       sliderInput("c9", "Full Currency Adjustment (%)",
-                  min=0, max=100, value=70),
+                  min=0, max=100, value=100),
       sliderInput("c10", "Import Costs Passed on to Consumers (%)",
-                  min=0, max=100, value=100))
+                  min=0, max=100, value=0))
     ),
     column(5,
            wellPanel(
       sliderInput("c5", "Domestic Sales / Total Sales (%)",
-                  min=0, max=100, value=100),
+                  min=0, max=100, value=50),
       sliderInput("c6", "Total Costs / Total Sales (%)",
-                  min=0, max=100, value=60),
+                  min=0, max=100, value=50),
       sliderInput("c7", "Domestic Costs / Total Costs (%)",
-                  min=0, max=100, value=90)
+                  min=0, max=100, value=50)
       ),
       strong("Table Keys:"),
       h5("OTR: old tax rate (35%)"),
