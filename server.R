a1a20f880f8665f757bcd28071cc1d1ce0721f0b
Sean <zhwa@umich.edu>
2016-12-22 14:56:21 -0600

    added max function

--- server.R
+++ server.R
@@ -11,13 +11,14 @@ function(input, output) {
                "Taxes",
                "After Tax Profit"),
       "OTR" = as.character(c(
-                                   input$c5,
-                                   (1-input$c5/100)*100, 
-                                   input$c6*input$c7/100,
-                                   (1-input$c7/100)*input$c6,
-                                   input$c5+(1-input$c5/100)*100-input$c6*input$c7/100-(1-input$c7/100)*input$c6,
-                                   0.35*(input$c5+(1-input$c5/100)*100-input$c6*input$c7/100-(1-input$c7/100)*input$c6),
-                                   input$c5+(1-input$c5/100)*100-input$c6*input$c7/100-(1-input$c7/100)*input$c6 - 0.35*(input$c5+(1-input$c5/100)*100-input$c6*input$c7/100-(1-input$c7/100)*input$c6))),
+                                   round(input$c5,digits = 2),
+                                   round((1-input$c5/100)*100,digits = 2), 
+                                   round(input$c6*input$c7/100,digits = 2),
+                                   round((1-input$c7/100)*input$c6,digits = 2),
+                                   round(input$c5+(1-input$c5/100)*100-input$c6*input$c7/100-(1-input$c7/100)*input$c6,digits = 2),
+                                   round(0.35*(input$c5+(1-input$c5/100)*100-input$c6*input$c7/100-(1-input$c7/100)*input$c6),digits = 2),
+                                   round(input$c5+(1-input$c5/100)*100-input$c6*input$c7/100-(1-input$c7/100)*input$c6 - 
+                                           0.35*(input$c5+(1-input$c5/100)*100-input$c6*input$c7/100-(1-input$c7/100)*input$c6))),digits = 2),
       
       "NTR" = as.character(c(round(input$c5,digits = 2),
                                    round((1-input$c5/100)*100,digits = 2),
@@ -38,26 +39,26 @@ function(input, output) {
                                                                                                         round((1-input$c5/100)*100,digits = 2)-
                                                                                                         round(input$c6*input$c7/100,digits = 2)-
                                                                                                         round((1-input$c7/100)*input$c6,digits = 2)),digits = 2))),
-                          
-     "NTR.BA" = as.character(c(  round(input$c5 + (((1-input$c7/100)*input$c6*(1-input$c9/100))-
-                                                    ((1-input$c5/100)*100*(1-input$c9/100)))*input$c8*(1/(1-input$c8/100))*input$c10/10000,digits =2),
+      
+     "NTR.BA" = as.character(c(  round(input$c5 + max((((1-input$c7/100)*input$c6*(1-input$c9/100))-
+                                                    ((1-input$c5/100)*100*(1-input$c9/100)))*input$c8*(1/(1-input$c8/100))*input$c10/10000,0),digits =2),
                                   round((1-input$c5/100)*100*(1-input$c8*input$c9/10000),digits = 2),
                                   round(input$c6*input$c7/100,digits = 2),
                                   round((1-input$c7/100)*input$c6*(1-input$c8*input$c9/10000),digits = 2),
-                               round(input$c5 + (((1-input$c7/100)*input$c6*(1-input$c9/100))-
-                                                         ((1-input$c5/100)*100*(1-input$c9/100)))*input$c8*(1/(1-input$c8/100))*input$c10/10000,digits =2) 
-                                     - round(input$c6*input$c7/100,digits = 2),
-                                  round(0.01*input$c8*(round(input$c5 + (((1-input$c7/100)*input$c6*(1-input$c9/100))-
-                                                                           ((1-input$c5/100)*100*(1-input$c9/100)))*input$c8*(1/(1-input$c8/100))*input$c10/10000,digits =2) 
+                               round(round(input$c5 + max((((1-input$c7/100)*input$c6*(1-input$c9/100))-
+                                                         ((1-input$c5/100)*100*(1-input$c9/100)))*input$c8*(1/(1-input$c8/100))*input$c10/10000,0),digits =2) 
+                                     - round(input$c6*input$c7/100,digits = 2),digits = 2),
+                                  round(0.01*input$c8*(round(input$c5 + max((((1-input$c7/100)*input$c6*(1-input$c9/100))-
+                                                                           ((1-input$c5/100)*100*(1-input$c9/100)))*input$c8*(1/(1-input$c8/100))*input$c10/10000,0),digits =2) 
                                                        - round(input$c6*input$c7/100,digits = 2)),digits = 2),
-                               round(input$c5 + (((1-input$c7/100)*input$c6*(1-input$c9/100))-
-                                              ((1-input$c5/100)*100*(1-input$c9/100)))*input$c8*(1/(1-input$c8/100))*input$c10/10000,digits =2)+
+                               round(round(input$c5 + max((((1-input$c7/100)*input$c6*(1-input$c9/100))-
+                                              ((1-input$c5/100)*100*(1-input$c9/100)))*input$c8*(1/(1-input$c8/100))*input$c10/10000,0),digits =2)+
                                round((1-input$c5/100)*100*(1-input$c8*input$c9/10000),digits = 2) -
                                  round(input$c6*input$c7/100,digits = 2)-
                                  round((1-input$c7/100)*input$c6*(1-input$c8*input$c9/10000),digits = 2) -
-                                 round(0.01*input$c8*(round(input$c5 + (((1-input$c7/100)*input$c6*(1-input$c9/100))-
-                                      ((1-input$c5/100)*100*(1-input$c9/100)))*input$c8*(1/(1-input$c8/100))*input$c10/10000,digits =2) 
-                                              - round(input$c6*input$c7/100,digits = 2)),digits = 2)
+                                 round(0.01*input$c8*(round(input$c5 + max((((1-input$c7/100)*input$c6*(1-input$c9/100))-
+                                      ((1-input$c5/100)*100*(1-input$c9/100)))*input$c8*(1/(1-input$c8/100))*input$c10/10000,0),digits =2) 
+                                              - round(input$c6*input$c7/100,digits = 2)),digits = 2),digits=2)
                                   )), 
       stringsAsFactors=FALSE)
   }) 
