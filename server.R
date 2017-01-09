58f346560d81558422dd7d41d05bc33d0fb24d4c
Sean <zhwa@umich.edu>
2017-01-09 10:08:41 +0800

    add more documentation

--- server.R
+++ server.R
@@ -11,30 +11,56 @@ function(input, output) {
                "Taxes",
                "After Tax Profit"),
       "OTR" = as.character(c(
-                round(input$dom_sales,digits = 1), # US Sales
-                round((1-input$dom_sales/100)*100,digits = 1), # Foreign Sales
-                round(input$tot_sales*input$dom_costs/100,digits = 1), # US Costs 
-                round((1-input$dom_costs/100)*input$tot_sales,digits = 1), # Foreign Costs
-                round(input$dom_sales+(1-input$dom_sales/100)*100  # Tax Base
+                # US Sales
+                round(input$dom_sales,digits = 1),
+
+                # Foreign Sales
+                round((1-input$dom_sales/100)*100,digits = 1), 
+
+                # US Costs 
+                round(input$tot_sales*input$dom_costs/100,digits = 1), 
+
+                # Foreign Costs
+                round((1-input$dom_costs/100)*input$tot_sales,digits = 1),
+
+                # Tax Base = (US Sales + Foreign Sales) - (US Costs + Foreign Costs)
+                round(input$dom_sales+(1-input$dom_sales/100)*100 
                   -input$tot_sales*input$dom_costs/100-(1-input$dom_costs/100)*input$tot_sales,digits = 1),
-                round(0.01*35*((input$dom_sales)+((1-input$dom_sales/100)*100) # Taxes
+
+                # Taxes
+                round(0.01*35*((input$dom_sales)+((1-input$dom_sales/100)*100)
                   -(input$tot_sales*input$dom_costs/100)-((1-input$dom_costs/100)*input$tot_sales)),digits = 1),
-                round((input$dom_sales+(1-input$dom_sales/100)*100 - # After Tax Profit
+
+                # For original tax rate, after Tax Profit = (Tax Base - Taxes)
+                round((input$dom_sales+(1-input$dom_sales/100)*100 - 
                   input$tot_sales*input$dom_costs/100-(1-input$dom_costs/100)*input$tot_sales- 
                     (0.01*35*(input$dom_sales+(1-input$dom_sales/100)*100-
                       input$tot_sales*input$dom_costs/100-(1-input$dom_costs/100)*input$tot_sales)))*10)/10
                 )),
       
       "NTR" = as.character(c(
-                round(input$dom_sales,digits = 1), # US Sales
-                round((1-input$dom_sales/100)*100,digits = 1), # Foreign Sales
-                round(input$tot_sales*input$dom_costs/100,digits = 1), # US Costs 
-                round((1-input$dom_costs/100)*input$tot_sales,digits = 1), # Foreign Costs
-                round(input$dom_sales + (1-input$dom_sales/100)*100 - # Tax Base
+                # US Sales
+                round(input$dom_sales,digits = 1),
+
+                # Foreign Sales
+                round((1-input$dom_sales/100)*100,digits = 1),
+
+                # US Costs 
+                round(input$tot_sales*input$dom_costs/100,digits = 1),
+
+                # Foreign Costs
+                round((1-input$dom_costs/100)*input$tot_sales,digits = 1),
+
+                # Tax Base = (US Sales + Foreign Sales) - (US Costs + Foreign Costs)
+                round(input$dom_sales + (1-input$dom_sales/100)*100 -
                   input$tot_sales*input$dom_costs/100- (1-input$dom_costs/100)*input$tot_sales,digits = 1),
-                round(0.01*input$new_tax_rate*(input$dom_sales + (1-input$dom_sales/100)*100- # Taxes
+
+                # Taxes
+                round(0.01*input$new_tax_rate*(input$dom_sales + (1-input$dom_sales/100)*100-
                   input$tot_sales*input$dom_costs/100-(1-input$dom_costs/100)*input$tot_sales),digits = 1),
-                round((input$dom_sales + (1-input$dom_sales/100)*100 - # After Tax Profit
+
+                # For new tax rate , after Tax Profit = (Tax Base - Taxes)
+                round((input$dom_sales + (1-input$dom_sales/100)*100 - 
                   input$tot_sales*input$dom_costs/100 -
                     (1-input$dom_costs/100)*input$tot_sales - 0.01*input$new_tax_rate*(input$dom_sales+
                       (1-input$dom_sales/100)*100 - input$tot_sales*input$dom_costs/100-
@@ -42,18 +68,31 @@ function(input, output) {
                 )),
       
      "NTR.BA" = as.character(c( 
-                round(input$dom_sales + max((((1-input$dom_costs/100)*input$tot_sales*(1-input$currency_adj/100))- # US Sales
+                # US Sales
+                round(input$dom_sales + max((((1-input$dom_costs/100)*input$tot_sales*(1-input$currency_adj/100))-
                   ((1-input$dom_sales/100)*100*(1-input$currency_adj/100)))*input$new_tax_rate*(1/(1-input$new_tax_rate/100))*input$import_cost/10000,0),digits = 1),
-                round((1-input$dom_sales/100)*100*(1-input$new_tax_rate*input$currency_adj/10000),digits = 1), # Foreign Sales
-                round(input$tot_sales*input$dom_costs/100,digits = 1), # US Costs 
-                round((1-input$dom_costs/100)*input$tot_sales*(1-input$new_tax_rate*input$currency_adj/10000),digits = 1), # Foreign Costs
-                round(input$dom_sales + max((((1-input$dom_costs/100)*input$tot_sales*(1-input$currency_adj/100))- # Tax Base
+                
+                # Foreign Sales
+                round((1-input$dom_sales/100)*100*(1-input$new_tax_rate*input$currency_adj/10000),digits = 1),
+                
+                # US Costs 
+                round(input$tot_sales*input$dom_costs/100,digits = 1),
+                
+                # Foreign Costs
+                round((1-input$dom_costs/100)*input$tot_sales*(1-input$new_tax_rate*input$currency_adj/10000),digits = 1),
+                
+                # Tax Base = (US Sales - US Costs)
+                round(input$dom_sales + max((((1-input$dom_costs/100)*input$tot_sales*(1-input$currency_adj/100))- 
                   ((1-input$dom_sales/100)*100*(1-input$currency_adj/100)))*input$new_tax_rate*(1/(1-input$new_tax_rate/100))*input$import_cost/10000,0)
                     - input$tot_sales*input$dom_costs/100,digits = 1),
-                round(0.01*input$new_tax_rate*(input$dom_sales + max((((1-input$dom_costs/100)*input$tot_sales*(1-input$currency_adj/100))- # Taxes
+                
+                # Taxes
+                round(0.01*input$new_tax_rate*(input$dom_sales + max((((1-input$dom_costs/100)*input$tot_sales*(1-input$currency_adj/100))-
                   ((1-input$dom_sales/100)*100*(1-input$currency_adj/100)))*input$new_tax_rate*(1/(1-input$new_tax_rate/100))*input$import_cost/10000,0) 
-                    - input$tot_sales*input$dom_costs/100),digits = 1),               
-                round(input$dom_sales + max((((1-input$dom_costs/100)*input$tot_sales*(1-input$currency_adj/100))- # After Tax Profit
+                    - input$tot_sales*input$dom_costs/100),digits = 1), 
+                
+                # For new tax rate with border adjustment, after Tax Profit = (US Sales + Foreign Sales) - (US Costs + Foreign Costs) - Taxes       
+                round(input$dom_sales + max((((1-input$dom_costs/100)*input$tot_sales*(1-input$currency_adj/100))-
                   ((1-input$dom_sales/100)*100*(1-input$currency_adj/100)))*input$new_tax_rate*(1/(1-input$new_tax_rate/100))*input$import_cost/10000,0) +
                     (1-input$dom_sales/100)*100*(1-input$new_tax_rate*input$currency_adj/10000) - input$tot_sales*input$dom_costs/100 -
                       (1-input$dom_costs/100)*input$tot_sales*(1-input$new_tax_rate*input$currency_adj/10000) -
