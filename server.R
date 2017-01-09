b05ca342a5ec12f83448643ff57883ee12408cf2
Sean <zhwa@umich.edu>
2017-01-09 11:02:12 +0800

    add detailed raw calculations for NTR.BA column

--- server.R
+++ server.R
@@ -68,17 +68,18 @@ function(input, output) {
                 )),
       
      "NTR.BA" = as.character(c( 
-                # US Sales
+                # US Sales = Domestic Sales + Max(((1 - Domestic Costs) * 100 * Total Sales * (1 - Currency Adjustment) - 
+                # (1 - Domestic Sales) * 100 * (1 - Currency Adjustment)) * New Tax Rate * (1/(1 - New Tax Rate)) * Import Cost , 0) 
                 round(input$dom_sales + max((((1-input$dom_costs/100)*input$tot_sales*(1-input$currency_adj/100))-
                   ((1-input$dom_sales/100)*100*(1-input$currency_adj/100)))*input$new_tax_rate*(1/(1-input$new_tax_rate/100))*input$import_cost/10000,0),digits = 1),
                 
-                # Foreign Sales
+                # Foreign Sales = (1 - Domestic Sales) * 100 * (1 - New Tax Rate * Currency Adjustment)
                 round((1-input$dom_sales/100)*100*(1-input$new_tax_rate*input$currency_adj/10000),digits = 1),
                 
-                # US Costs 
+                # US Costs = 100 * Total Sales * Domestic Costs
                 round(input$tot_sales*input$dom_costs/100,digits = 1),
                 
-                # Foreign Costs
+                # Foreign Costs = (1 - Domestic Costs) * 100 * Total Sales * (1 - New Tax Rate * Currency Adjustment)
                 round((1-input$dom_costs/100)*input$tot_sales*(1-input$new_tax_rate*input$currency_adj/10000),digits = 1),
                 
                 # Tax Base = (US Sales - US Costs)
@@ -86,7 +87,7 @@ function(input, output) {
                   ((1-input$dom_sales/100)*100*(1-input$currency_adj/100)))*input$new_tax_rate*(1/(1-input$new_tax_rate/100))*input$import_cost/10000,0)
                     - input$tot_sales*input$dom_costs/100,digits = 1),
                 
-                # Taxes
+                # Taxes = New Tax Rate * Tax Base
                 round(0.01*input$new_tax_rate*(input$dom_sales + max((((1-input$dom_costs/100)*input$tot_sales*(1-input$currency_adj/100))-
                   ((1-input$dom_sales/100)*100*(1-input$currency_adj/100)))*input$new_tax_rate*(1/(1-input$new_tax_rate/100))*input$import_cost/10000,0) 
                     - input$tot_sales*input$dom_costs/100),digits = 1), 
