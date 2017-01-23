4190af93349343b2651c5df63b1afbedf538591a
Sean <zhwa@umich.edu>
2017-01-23 14:05:51 +0800

    added imtermediate functions

--- tests/test-numeric.R
+++ tests/test-numeric.R
@@ -1,3 +1,44 @@
+OTR_TB <- function(dom_costs, dom_sales, tot_sales){
+    return(round(dom_sales+(1-dom_sales/100)*100 
+                  -tot_sales*dom_costs/100-(1-dom_costs/100)*tot_sales,digits = 1))
+}
+
+OTR_ATP <- function(dom_costs, dom_sales, tot_sales){
+    return(round((dom_sales+(1-dom_sales/100)*100 - 
+                  tot_sales*dom_costs/100-(1-dom_costs/100)*tot_sales- 
+                    (0.01*35*(dom_sales+(1-dom_sales/100)*100-
+                      tot_sales*dom_costs/100-(1-dom_costs/100)*tot_sales)))*10)/10
+    )
+}
+
+NTR_TB <- function(dom_costs, dom_sales, tot_sales){
+    return(round(dom_sales + (1-dom_sales/100)*100 -
+                  tot_sales*dom_costs/100- (1-dom_costs/100)*tot_sales,digits = 1)
+    )
+}
+
+NTR_ATP <- function(dom_costs, dom_sales, tot_sales, new_tax_rate){
+    return(round((dom_sales + (1-dom_sales/100)*100 - 
+                  tot_sales*dom_costs/100 -
+                    (1-dom_costs/100)*tot_sales - 0.01*new_tax_rate*(dom_sales+
+                      (1-dom_sales/100)*100 - tot_sales*dom_costs/100-
+                        (1-dom_costs/100)*tot_sales))*10)/10
+    )
+}
+
+NTR_BA_USS <- function(dom_costs, dom_sales, tot_sales, currency_adj, import_cost, new_tax_rate){
+    return(round(dom_sales + max((((1-dom_costs/100)*tot_sales*(1-currency_adj/100))-
+                  ((1-dom_sales/100)*100*(1-currency_adj/100)))*new_tax_rate*(1/(1-new_tax_rate/100))*import_cost/10000,0),digits = 1)
+    )
+}
+
+NTR_BA_TB <- function(dom_costs, dom_sales, tot_sales, currency_adj, import_cost, new_tax_rate){
+    return(round(dom_sales + max((((1-dom_costs/100)*tot_sales*(1-currency_adj/100))- 
+                  ((1-dom_sales/100)*100*(1-currency_adj/100)))*new_tax_rate*(1/(1-new_tax_rate/100))*import_cost/10000,0)
+                    - tot_sales*dom_costs/100,digits = 1)
+    )
+}
+
 NTR_BA_ATP <- function(dom_costs, dom_sales, tot_sales, currency_adj, import_cost, new_tax_rate){
     return(round(dom_sales + max((((1-dom_costs/100)*tot_sales*(1-currency_adj/100))-
                                ((1-dom_sales/100)*100*(1-currency_adj/100)))*new_tax_rate*(1/(1-new_tax_rate/100))*import_cost/10000,0) +
