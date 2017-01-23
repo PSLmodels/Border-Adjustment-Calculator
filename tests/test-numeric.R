0d24b741f48afd7acf9dca211c1361faf0614aba
Sean <zhwa@umich.edu>
2017-01-23 23:16:04 +0800

    revise and add more tests

--- tests/test-numeric.R
+++ tests/test-numeric.R
@@ -49,9 +49,50 @@ NTR_BA_ATP <- function(dom_costs, dom_sales, tot_sales, currency_adj, import_cos
                                   - tot_sales*dom_costs/100)),1))
 }
 
-test_that("NTR.BA after tax profit", {  
-    results = NTR_BA_ATP(20,50,30,80,55,25)
-    expect_that( results, is_a("numeric") )
-    expect_that( length(results), equals(1) )
-    expect_that( results == 53.8, is_true() )
+test_that("Testing Original Tax Rate #1", {  
+  result_TB = OTR_TB(7,0,25)
+  result_ATP = OTR_ATP(7,0,25)
+  expect_that( result_TB, is_a("numeric") )
+  expect_that( result_ATP, is_a("numeric") )
+  expect_that( length(result_TB), equals(1) )
+  expect_that( length(result_ATP), equals(1) )
+  expect_that( result_TB == 75, is_true() )
+  expect_that( result_ATP == 48.8, is_true() )
+})
+
+test_that("Testing New Tax Rate #2", {  
+  result_TB = NTR_TB(71,27,95)
+  result_ATP = NTR_ATP(71,27,95,41)
+  expect_that( result_TB, is_a("numeric") )
+  expect_that( result_ATP, is_a("numeric") )
+  expect_that( length(result_TB), equals(1) )
+  expect_that( length(result_ATP), equals(1) )
+  expect_that( result_TB == 5, is_true() )
+  expect_that( result_ATP == 2.9, is_true() )
+})
+
+test_that("Testing New Tax Rate #3", {  
+  result_TB = NTR_TB(93,23,37)
+  result_ATP = NTR_ATP(93,23,37,87)
+  expect_that( result_TB, is_a("numeric") )
+  expect_that( result_ATP, is_a("numeric") )
+  expect_that( length(result_TB), equals(1) )
+  expect_that( length(result_ATP), equals(1) )
+  expect_that( result_TB == 63, is_true() )
+  expect_that( result_ATP == 8.2, is_true() )
+})
+
+test_that("Testing New Tax Rate with Border Adjustment #4", {  
+  result_USS = NTR_BA_USS(28,58,83,80,27,27)
+  result_TB = NTR_BA_TB(28,58,83,80,27,27)
+  result_ATP = NTR_BA_ATP(28,58,83,80,27,27)
+  expect_that( result_TB, is_a("numeric") )
+  expect_that( result_ATP, is_a("numeric") )
+  expect_that( result_USS, is_a("numeric") )
+  expect_that( length(result_TB), equals(1) )
+  expect_that( length(result_ATP), equals(1) )
+  expect_that( length(result_USS), equals(1) )
+  expect_that( result_USS == 58.4, is_true() )
+  expect_that( result_TB == 35.1, is_true() )
+  expect_that( result_ATP == 11.7, is_true() )
 })
